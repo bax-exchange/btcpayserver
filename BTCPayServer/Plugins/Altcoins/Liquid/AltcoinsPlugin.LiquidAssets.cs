92dc810d0f000cbe24477a741a233f56a99445f9
Omar Baqueiro <omar@bax.mx>
2025-01-17 16:25:05 -0600

    Fix mexas image

--- BTCPayServer/Plugins/Altcoins/Liquid/AltcoinsPlugin.LiquidAssets.cs
+++ BTCPayServer/Plugins/Altcoins/Liquid/AltcoinsPlugin.LiquidAssets.cs
@@ -66,7 +66,7 @@ public partial class AltcoinsPlugin
             AssetId = new uint256("0e99c1a6da379d1f4151fb9df90449d40d0608f6cb33a5bcbfc8c265f42bab0a"),
             DisplayName = "Liquid CAD",
             NBXplorerNetwork = nbxplorerNetwork,
-            CryptoImagePath = "imlegacy/lcad.png",
+            CryptoImagePath = "imlegacy/mexas.png",
             DefaultSettings = BTCPayDefaultSettings.GetDefaultSettings(ChainName),
             CoinType = ChainName == ChainName.Mainnet ? new KeyPath("1776'") : new KeyPath("1'"),
             SupportRBF = true,            
